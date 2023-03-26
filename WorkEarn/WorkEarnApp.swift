76c27e27e7e4553ba49e16cc0332b204ac3c5195
Berna <lucabusinesswork1@gmail.com>
2023-03-26 21:31:38 +0200

    Initial Commit

---
+++ WorkEarn/WorkEarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkEarnApp.swift
+//  WorkEarn
+//
+//  Created by Luca Bernardis on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkEarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
